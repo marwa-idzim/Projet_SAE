d372b72e66b04482295abfc9d48c7b4a32db78bf
marwa-idzim <idzim.marwa@gmail.com>
2026-06-12 14:10:08 +0100

    Ajout connexion base de données et page d'accueil

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<?php
+require "../config/db.php";
+echo "Connexion réussie";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Association sportive</title>
+</head>
+<body>
+    <h1>Description de la base de données</h1>
+    <p>Ceci est la description de la base de données (en cours de rédaction)</p>
+    <a href="adherents.php">consulter la liste des adherents</a>
+    <a href="sport.php">consulter la liste des sports</a>
+    <a href="inscriptions.php">consulter la liste des inscriptions</a>
+    <a href="seances.php">consulter la liste des seances</a>
+
+
+    
+</body>
+</html>
